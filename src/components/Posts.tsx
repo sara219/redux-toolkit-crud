7ed0d51226bdedd489ab0f8a928112b875785b21
sara219 <sara97abuawwad@gmail.com>
2023-02-18 20:39:56 +0200

    handle input change

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addPost } from '../redux/postsReducer'
+
 export const Posts = () => {
+  const [title, setTitle] = useState<string>('')
+  const [desc, setDesc] = useState<string>('')
+
+  const dispatch = useDispatch()
+
+  const handleChangeTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+  }
+
+  const handleChangeDesc = (e: ChangeEvent<HTMLInputElement>) => {
+    setDesc(e.target.value)
+  }
+
+  const addPostItem = () => {
+    dispatch(addPost({ id: 1, title, desc}))
+  }
+
   return (
     <div>
       <div className='form'>
-        <input type='text' placeholder='Enter Title' />
-        <input type='text' placeholder='Enter Description' />
-        <button>ADD YOU POST</button>
+        <input
+          type='text'
+          placeholder='Enter Title'
+          value={title}
+          onChange={handleChangeTitle}
+        />
+        <input
+          type='text'
+          placeholder='Enter Description'
+          value={desc}
+          onChange={handleChangeDesc}
+        />
+        <button onClick={addPostItem}>ADD YOU POST</button>
       </div>
 
       <div className='posts'>
