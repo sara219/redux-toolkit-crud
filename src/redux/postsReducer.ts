5b297ea22360e187afc8c48f2ba1bbe7018dae79
sara219 <sara97abuawwad@gmail.com>
2023-02-18 20:40:18 +0200

    create action for reducer

--- src/redux/postsReducer.ts
+++ src/redux/postsReducer.ts
@@ -14,9 +14,13 @@ export const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-
+        addPost: function(state, action){
+            console.log(action,1111);
+            
+            // state.post.push()
+        }
     }
 })
 
-
+export const { addPost } = postsSlice.actions
 export default postsSlice.reducer
