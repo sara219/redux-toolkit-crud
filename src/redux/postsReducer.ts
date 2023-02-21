fdcc38ad267c277e2b817ce45341ce858fcd5a30
sara219 <sara97abuawwad@gmail.com>
2023-02-21 21:42:34 +0200

    update post post

--- src/redux/postsReducer.ts
+++ src/redux/postsReducer.ts
@@ -19,8 +19,16 @@ export const postsSlice = createSlice({
     deletePost: function(state, action: PayloadAction<number>) {
       state.items = state.items.filter((post) => post.id !== action.payload)
     },
+    updatePost: function(state, action) {
+      state.items.map((item) => {
+        if (item.id === action.payload.id) {
+          item.title = action.payload.title
+          item.desc = action.payload.desc
+        }
+      })
+    },
   },
 })
 
-export const { addPost, deletePost } = postsSlice.actions
+export const { addPost, deletePost, updatePost } = postsSlice.actions
 export default postsSlice.reducer
